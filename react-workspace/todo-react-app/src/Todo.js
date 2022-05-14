25c59a6efc63163eef54a958ce3afa4e55758780
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-14 21:13:21 +0900

    Create a function that delete Todo at the List

--- react-workspace/todo-react-app/src/Todo.js
+++ react-workspace/todo-react-app/src/Todo.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+import { ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton } from "@material-ui/core";
+import { DeleteOutlined } from '@material-ui/icons';
 
 class Todo extends React.Component {
     constructor(props){
         super(props);   // props 오브젝트 초기화
         this.state = { item: props.item };
+        this.delete = props.delete;
+    }
+
+    // 삭제처리 핸들러 추가
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
     }
 
     render() {
     const item = this.state.item;
     return (
         <ListItem>
-            <Checkbox checked={item.done} />
+            <Checkbox checked={item.done} disableRipple />
             <ListItemText>
                 <InputBase
                     inputProps={{ "aria-label": "naked" }}
@@ -23,6 +30,14 @@ class Todo extends React.Component {
                     fullWidth={true}
                 />
             </ListItemText>
+
+            <ListItemSecondaryAction>
+                <IconButton
+                    aria-label="Delete Todo"
+                    onClick={this.deleteEventHandler}>
+                    <DeleteOutlined />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
         );
     }
